99ec0b006b72f80f23c5bc2105ff01be6f43525f
Emil Dimitrov <edimitrov1@gmail.com>
2020-07-06 00:42:47 +0300

    Implement product endpoints

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,43 +1,85 @@
-exports.getCategories = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: 0,
-    data: {
-      categories: [],
-    },
-  });
+const Category = require('../models/Category');
+
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: categories.length,
+      data: { categories },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.getCateogry = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      category: {},
-    },
-  });
+exports.getCateogry = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: { category },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.createCategory = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      category: null,
-    },
-  });
+exports.createCategory = async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { category: newCategory },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.updateCateogry = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      category: {},
-    },
-  });
+exports.updateCateogry = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { category },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.deleteCateogry = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteCateogry = async (req, res) => {
+  try {
+    await Category.findByIdAndRemove(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,83 @@
-exports.getProducts = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Not yet implemented',
-  });
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: { products },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
-exports.getProduct = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Not yet implemented',
-  });
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: { product },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
-exports.createProduct = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Not yet implemented',
-  });
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { product: newProduct },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.updateProduct = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Not yet implemented',
-  });
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { product },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
-exports.deleteProduct = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Not yet implemented',
-  });
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndRemove(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
